2791593b40678e527808c928a7abb161a0c38faa
mccrush <mccrush2027@gmail.com>
2022-10-23 19:54:48 +0700

    Settings work show Group list

--- src/components/GroupItem.vue
+++ src/components/GroupItem.vue
@@ -1,11 +1,22 @@
 <template>
-  <li class="list-group-item">
-    {{ group.title }}
+  <li
+    class="list-group-item d-flex justify-content-between align-items-center"
+    :class="{
+      'list-group-item-danger': group.type === 'credit',
+      'list-group-item-success': group.type === 'debet'
+    }"
+  >
+    <span>{{ group.title }}</span>
+    <div>
+      <ButtonRemove />
+    </div>
   </li>
 </template>
 
 <script>
+import ButtonRemove from './buttons/ButtonRemove.vue'
 export default {
+  components: { ButtonRemove },
   props: {
     group: Object
   }
